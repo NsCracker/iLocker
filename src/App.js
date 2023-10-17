262037b2590b05d67ffc1eca72e23eb385dadf22
Ns Cracker <115659007+NsCracker@users.noreply.github.com>
2023-10-17 23:30:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/NoteState';
+import { Alert } from './components/Alert';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-
-
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
